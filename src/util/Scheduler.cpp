247d0b542d8b0573b62f6477d5f195734b1a4ec7
NoobLLiu <nooblliu@users.noreply.github.com>
2026-07-19 18:00:09 +0800

    fix: ServerThreadExecutor::executeAfter now requires Duration (nanoseconds) instead of raw tick int

--- src/util/Scheduler.cpp
+++ src/util/Scheduler.cpp
@@ -2,6 +2,8 @@
 
 #include "ll/api/thread/ServerThreadExecutor.h"
 
+#include <chrono>
+
 namespace welcome_noob {
 
 void Scheduler::after(int ticks, std::function<void()> cb) {
@@ -9,7 +11,12 @@ void Scheduler::after(int ticks, std::function<void()> cb) {
         now(std::move(cb));
         return;
     }
-    ll::thread::ServerThreadExecutor::getDefault().executeAfter(std::move(cb), ticks);
+    // LL 26.10.14: executeAfter 第二参数为 Duration (std::chrono::nanoseconds)
+    // 1 game tick = 50ms = 50'000'000 ns
+    ll::thread::ServerThreadExecutor::getDefault().executeAfter(
+        std::move(cb),
+        std::chrono::nanoseconds(ticks * 50'000'000LL)
+    );
 }
 
 void Scheduler::now(std::function<void()> cb) {
